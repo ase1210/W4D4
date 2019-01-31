eeaabd6b4d842d8cd67eeada032085af77333ac1
DrewE <andrew.engelstein@gmail.com>
2019-01-31 15:28:13 -0800

    Add #show, #create, #new and private #user_params to UsersController

--- MusicApp/app/controllers/users_controller.rb
+++ MusicApp/app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new             #need view for new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages  #need to add errors to new view or template view
+      render :new
+    end
+  end
+
+  def show
+    render :show            #need view for show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
